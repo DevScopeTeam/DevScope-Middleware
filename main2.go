877114cea4a7b56dc267cc09136c99fcc77a6679
zjy2414 <zjy2414@outlook.com>
2024-11-03 00:10:42 +0800

    fix: main.go

--- main2.go
+++ main2.go
@@ -1,16 +1,16 @@
 package main
 
 // func main() {
-// 	// fmt.Println(method.CalculateWorkWeightInRepo("stulzq", "dotnetcore", "CanalSharp"))
-// 	fmt.Println(method.CalculateDeveloperScore("zjy2414"))
+// fmt.Println(method.CalculateWorkWeightInRepo("stulzq", "dotnetcore", "CanalSharp"))
+// fmt.Println(method.CalculateDeveloperScore("zjy2414"))
 
-// 	// fmt.Println(method.GetUserEvents("morri3"))
-// 	fmt.Println("***********")
-// 	// repos, err := method.GetUserRecentRepos("rsonghuster")
-// 	// if err != nil {
-// 	// 	fmt.Println(err)
-// 	// }
-// 	// for _, repo := range repos {
-// 	// 	fmt.Println(repo.Owner.Login, "|", repo.Name)
-// 	// }
+// fmt.Println(method.GetUserEvents("morri3"))
+// fmt.Println("***********")
+// repos, err := method.GetUserRecentRepos("rsonghuster")
+// if err != nil {
+// 	fmt.Println(err)
+// }
+// for _, repo := range repos {
+// 	fmt.Println(repo.Owner.Login, "|", repo.Name)
+// }
 // }
