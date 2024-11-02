09fd6a3fe62e1f9869980ab6da3c175d75974bb0
zjy2414 <zjy2414@outlook.com>
2024-11-02 15:08:50 +0800

    fix: main function

--- main2.go
+++ main2.go
@@ -1,21 +1,16 @@
 package main
 
-import (
-	"DevScope-Middleware/method"
-	"fmt"
-)
+// func main() {
+// 	// fmt.Println(method.CalculateWorkWeightInRepo("stulzq", "dotnetcore", "CanalSharp"))
+// 	fmt.Println(method.CalculateDeveloperScore("zjy2414"))
 
-func main() {
-	// fmt.Println(method.CalculateWorkWeightInRepo("stulzq", "dotnetcore", "CanalSharp"))
-	fmt.Println(method.CalculateDeveloperScore("zjy2414"))
-
-	// fmt.Println(method.GetUserEvents("morri3"))
-	fmt.Println("***********")
-	// repos, err := method.GetUserRecentRepos("rsonghuster")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// for _, repo := range repos {
-	// 	fmt.Println(repo.Owner.Login, "|", repo.Name)
-	// }
-}
+// 	// fmt.Println(method.GetUserEvents("morri3"))
+// 	fmt.Println("***********")
+// 	// repos, err := method.GetUserRecentRepos("rsonghuster")
+// 	// if err != nil {
+// 	// 	fmt.Println(err)
+// 	// }
+// 	// for _, repo := range repos {
+// 	// 	fmt.Println(repo.Owner.Login, "|", repo.Name)
+// 	// }
+// }
